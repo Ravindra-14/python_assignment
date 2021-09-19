ff81b1408dfec61a70abbc49806064c4701dc306
Ravindra <60481065+Ravindra-14@users.noreply.github.com>
2021-09-19 09:57:06 +0530

    Add files via upload

---
+++ assi+1.py
@@ -0,0 +1,9 @@
+length=5
+breadth=10
+area=length*breadth
+perimeter=2*(length+breadth)
+if(area<perimeter):
+    print("area of rectangle is greater")
+else:
+    print("perimeter of rectangle is greater")
+    
